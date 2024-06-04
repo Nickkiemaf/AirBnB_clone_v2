c20d87e6a71c1c1188e4fd23a2396d6496eb91b9
Nickkiemaf <mikailajibola1424@gmail.com>
2024-06-04 14:46:25 -0700

    web flask

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+from flask import Flask, escape
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """Route '/' to display 'Hello HBNB!'."""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def display_hbnb():
+    """Route '/hbnb' to display 'HBNB'."""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def display_c_text(text):
+    """Route '/c/<text>' to display 'C ' followed by the value of the text variable."""
+    return "C {}".format(escape(text.replace("_", " ")))
+
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def display_python_text(text):
+    """Route '/python/<text>' to display 'Python ' followed by the value of the text variable."""
+    return "Python {}".format(escape(text.replace("_", " ")))
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
